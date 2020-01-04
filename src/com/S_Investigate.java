c45e04e375d0d7dad5bb81d06caaffcb7d807ced
Darling-yyj <3047988426@qq.com>
2020-01-04 12:26:05 +0800

    123

---
+++ src/com/S_Investigate.java
@@ -0,0 +1,54 @@
+package com.entity;
+
+public class S_Investigate {
+	
+	public int getRpt_no() {
+		return rpt_no;
+	}
+	public void setRpt_no(int rpt_no) {
+		this.rpt_no = rpt_no;
+	}
+	public String getRpt_name() {
+		return rpt_name;
+	}
+	public void setRpt_name(String rpt_name) {
+		this.rpt_name = rpt_name;
+	}
+	public String getRpt_type() {
+		return rpt_type;
+	}
+	public void setRpt_type(String rpt_type) {
+		this.rpt_type = rpt_type;
+	}
+	public String getRpt_usage() {
+		return rpt_usage;
+	}
+	public void setRpt_usage(String rpt_usage) {
+		this.rpt_usage = rpt_usage;
+	}
+	public String getRpt_ym() {
+		return rpt_ym;
+	}
+	public void setRpt_ym(String rpt_ym) {
+		this.rpt_ym = rpt_ym;
+	}
+	public String getMake_time() {
+		return make_time;
+	}
+	public void setMake_time(String make_time) {
+		this.make_time = make_time;
+	}
+	public String getStatus_code() {
+		return status_code;
+	}
+	public void setStatus_code(String status_code) {
+		this.status_code = status_code;
+	}
+	private int rpt_no;
+	private String rpt_name;
+	private String rpt_type;
+	private String rpt_usage;
+	private String rpt_ym;
+	private String make_time;
+	private String status_code;
+}
