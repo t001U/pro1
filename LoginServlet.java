2dd8bfa25a6ca200c730996b9409796d0a008ac1
1413 <1413097437@qq.com>
2020-01-04 14:02:23 +0800

    sdfsdfd

---
+++ LoginServlet.java
@@ -0,0 +1,31 @@
+package com.java.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		resp.setCharacterEncoding("utf-8");
+		String name = req.getParameter("email");
+		PrintWriter out =resp.getWriter();
+		if (name.equals("123.com")) {
+			out.write("yes");
+		}else {
+			out.write("no");
+		}
+	}
+	
+}
