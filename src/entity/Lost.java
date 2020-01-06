0ea5ab8cc0a9bd40ec6b8214905dfea8baeb4abe
wdw1999 <3509631659@qq.com>
2020-01-06 11:20:58 +0800

    药品报损表实体类

---
+++ src/entity/Lost.java
@@ -0,0 +1,168 @@
+package com.entity;
+/**
+ * 药品报损表实体类
+ * @author Administrator
+ *
+ */
+public class Lost {
+
+	private int ID;
+	
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getCODE() {
+		return CODE;
+	}
+
+	public void setCODE(String cODE) {
+		CODE = cODE;
+	}
+
+	public String getDRUGID() {
+		return DRUGID;
+	}
+
+	public void setDRUGID(String dRUGID) {
+		DRUGID = dRUGID;
+	}
+
+	public String getCNAME() {
+		return CNAME;
+	}
+
+	public void setCNAME(String cNAME) {
+		CNAME = cNAME;
+	}
+
+	public String getSPEC() {
+		return SPEC;
+	}
+
+	public void setSPEC(String sPEC) {
+		SPEC = sPEC;
+	}
+
+	public String getUNIT() {
+		return UNIT;
+	}
+
+	public void setUNIT(String uNIT) {
+		UNIT = uNIT;
+	}
+
+	public int getJOBPRICE() {
+		return JOBPRICE;
+	}
+
+	public void setJOBPRICE(int jOBPRICE) {
+		JOBPRICE = jOBPRICE;
+	}
+
+	public int getSALEPRICE() {
+		return SALEPRICE;
+	}
+
+	public void setSALEPRICE(int sALEPRICE) {
+		SALEPRICE = sALEPRICE;
+	}
+
+	public int getNUMBER() {
+		return NUMBER;
+	}
+
+	public void setNUMBER(int nUMBER) {
+		NUMBER = nUMBER;
+	}
+
+	public String getPROVIDER() {
+		return PROVIDER;
+	}
+
+	public void setPROVIDER(String pROVIDER) {
+		PROVIDER = pROVIDER;
+	}
+
+	public int getTYPE() {
+		return TYPE;
+	}
+
+	public void setTYPE(int tYPE) {
+		TYPE = tYPE;
+	}
+
+	public String getTIMELIMIT() {
+		return TIMELIMIT;
+	}
+
+	public void setTIMELIMIT(String tIMELIMIT) {
+		TIMELIMIT = tIMELIMIT;
+	}
+
+	public String getEVENTTIME() {
+		return EVENTTIME;
+	}
+
+	public void setEVENTTIME(String eVENTTIME) {
+		EVENTTIME = eVENTTIME;
+	}
+
+	public String getBATCHID() {
+		return BATCHID;
+	}
+
+	public void setBATCHID(String bATCHID) {
+		BATCHID = bATCHID;
+	}
+
+	public String getCAUSE() {
+		return CAUSE;
+	}
+
+	public void setCAUSE(String cAUSE) {
+		CAUSE = cAUSE;
+	}
+
+	public String getINPUTCODE1() {
+		return INPUTCODE1;
+	}
+
+	public void setINPUTCODE1(String iNPUTCODE1) {
+		INPUTCODE1 = iNPUTCODE1;
+	}
+
+	private String CODE;
+	
+	private String DRUGID;
+	
+	private String CNAME;
+	
+	private String SPEC;
+	
+	private String UNIT;
+	
+	private int JOBPRICE;
+	
+	private int SALEPRICE;
+	
+	private int NUMBER;
+	
+	private String PROVIDER;
+	
+	private int TYPE;
+	
+	private String TIMELIMIT;
+	
+	private String EVENTTIME;
+	
+	private String BATCHID;
+	
+	private String CAUSE;
+	
+	private String INPUTCODE1;
+}
