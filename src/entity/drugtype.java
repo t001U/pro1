9385338b67ed521740a634c21d3bb25fe12b3e08
ttttt <1335496213@qq.com>
2020-01-06 13:50:29 +0800

    药品类型表

---
+++ src/entity/drugtype.java
@@ -0,0 +1,40 @@
+package com.Pro.entity;
+
+public class drugtype {
+
+	private int id;
+	private String code;
+	private String name;
+	private String INPUTCODE1;
+	private String DESCRIPTION;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getINPUTCODE1() {
+		return INPUTCODE1;
+	}
+	public void setINPUTCODE1(String iNPUTCODE1) {
+		INPUTCODE1 = iNPUTCODE1;
+	}
+	public String getDESCRIPTION() {
+		return DESCRIPTION;
+	}
+	public void setDESCRIPTION(String dESCRIPTION) {
+		DESCRIPTION = dESCRIPTION;
+	}
+}
