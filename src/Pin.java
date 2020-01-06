c867729fce15cb84c283d3b1046aaf1021467625
cf12 <1849764459@qq.com>
2020-01-06 13:41:52 +0800

    药品子点列表实体类

---
+++ src/Pin.java
@@ -0,0 +1,174 @@
+package com.Drug.entity;
+
+public class Pin {
+
+	private int id;
+	
+	private String code;
+	
+	private String drugId;
+	
+	private String cName;
+	
+	private String spec;
+	
+	private String unit;
+	
+	private double jobPrice;
+	
+	private double salePrice;
+	
+	private int number;
+	
+	private int type;
+	
+	private int downLimit;
+	
+	private int upLimit;
+	
+	private String timeLimit;
+	
+	private String eventTime;
+	
+	private String provider;
+	
+	private String batchId;
+	
+	private String inputcode1;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getDrugId() {
+		return drugId;
+	}
+
+	public void setDrugId(String drugId) {
+		this.drugId = drugId;
+	}
+
+	public String getcName() {
+		return cName;
+	}
+
+	public void setcName(String cName) {
+		this.cName = cName;
+	}
+
+	public String getSpec() {
+		return spec;
+	}
+
+	public void setSpec(String spec) {
+		this.spec = spec;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public double getJobPrice() {
+		return jobPrice;
+	}
+
+	public void setJobPrice(double jobPrice) {
+		this.jobPrice = jobPrice;
+	}
+
+	public double getSalePrice() {
+		return salePrice;
+	}
+
+	public void setSalePrice(double salePrice) {
+		this.salePrice = salePrice;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public int getDownLimit() {
+		return downLimit;
+	}
+
+	public void setDownLimit(int downLimit) {
+		this.downLimit = downLimit;
+	}
+
+	public int getUpLimit() {
+		return upLimit;
+	}
+
+	public void setUpLimit(int upLimit) {
+		this.upLimit = upLimit;
+	}
+
+	public String getTimeLimit() {
+		return timeLimit;
+	}
+
+	public void setTimeLimit(String timeLimit) {
+		this.timeLimit = timeLimit;
+	}
+
+	public String getEventTime() {
+		return eventTime;
+	}
+
+	public void setEventTime(String eventTime) {
+		this.eventTime = eventTime;
+	}
+
+	public String getProvider() {
+		return provider;
+	}
+
+	public void setProvider(String provider) {
+		this.provider = provider;
+	}
+
+	public String getBatchId() {
+		return batchId;
+	}
+
+	public void setBatchId(String batchId) {
+		this.batchId = batchId;
+	}
+
+	public String getInputcode1() {
+		return inputcode1;
+	}
+
+	public void setInputcode1(String inputcode1) {
+		this.inputcode1 = inputcode1;
+	}
+}
