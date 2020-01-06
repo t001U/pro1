f47e4597bd698639fa3fc82cb3624301e0179662
wdw1999 <3509631659@qq.com>
2020-01-06 11:19:50 +0800

    药品信息表实体类

---
+++ src/entity/Dict.java
@@ -0,0 +1,158 @@
+package com.entity;
+/**
+ * 药品信息表实体类
+ * @author Administrator
+ *
+ */
+public class Dict {
+	
+	private int ID;
+	
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getCODE() {
+		return CODE;
+	}
+
+	public void setCODE(String cODE) {
+		CODE = cODE;
+	}
+
+	public String getCNAME() {
+		return CNAME;
+	}
+
+	public void setCNAME(String cNAME) {
+		CNAME = cNAME;
+	}
+
+	public String getSPEC() {
+		return SPEC;
+	}
+
+	public void setSPEC(String sPEC) {
+		SPEC = sPEC;
+	}
+
+	public String getUNIT() {
+		return UNIT;
+	}
+
+	public void setUNIT(String uNIT) {
+		UNIT = uNIT;
+	}
+
+	public String getPRICE() {
+		return PRICE;
+	}
+
+	public void setPRICE(String pRICE) {
+		PRICE = pRICE;
+	}
+
+	public int getUPLIMIT() {
+		return UPLIMIT;
+	}
+
+	public void setUPLIMIT(int uPLIMIT) {
+		UPLIMIT = uPLIMIT;
+	}
+
+	public int getDOWNLIMIT() {
+		return DOWNLIMIT;
+	}
+
+	public void setDOWNLIMIT(int dOWNLIMIT) {
+		DOWNLIMIT = dOWNLIMIT;
+	}
+
+	public String getEFFECT() {
+		return EFFECT;
+	}
+
+	public void setEFFECT(String eFFECT) {
+		EFFECT = eFFECT;
+	}
+
+	public String getFILENO() {
+		return FILENO;
+	}
+
+	public void setFILENO(String fILENO) {
+		FILENO = fILENO;
+	}
+
+	public int getTYPE() {
+		return TYPE;
+	}
+
+	public void setTYPE(int tYPE) {
+		TYPE = tYPE;
+	}
+
+	public int getISOTC() {
+		return ISOTC;
+	}
+
+	public void setISOTC(int iSOTC) {
+		ISOTC = iSOTC;
+	}
+
+	public String getINPUTCODE1() {
+		return INPUTCODE1;
+	}
+
+	public void setINPUTCODE1(String iNPUTCODE1) {
+		INPUTCODE1 = iNPUTCODE1;
+	}
+
+	public String getREMARKS() {
+		return REMARKS;
+	}
+
+	public void setREMARKS(String rEMARKS) {
+		REMARKS = rEMARKS;
+	}
+
+	public int getISENABLE() {
+		return ISENABLE;
+	}
+
+	public void setISENABLE(int iSENABLE) {
+		ISENABLE = iSENABLE;
+	}
+
+	private String CODE;
+	
+	private String CNAME;
+	
+	private String SPEC;
+	
+	private String UNIT;
+	
+	private String PRICE;
+	
+	private int UPLIMIT;
+	
+	private int DOWNLIMIT;
+	
+	private String EFFECT;
+	
+	private String FILENO;
+	
+	private int TYPE;
+	
+	private int ISOTC;
+	
+	private String INPUTCODE1;
+	
+	private String REMARKS;
+	
+	private int ISENABLE;
+}
