ecc15711d0e2d769f86203e150e72f26d9cab771
cf12 <1849764459@qq.com>
2020-01-06 11:00:20 +0800

    hcf

---
+++ hcf/com/Drug/entity/Accountgrouping.java
@@ -0,0 +1,24 @@
+package com.Drug.entity;
+
+public class Accountgrouping {
+
+	private String loginId;
+	
+	public String getLoginId() {
+		return loginId;
+	}
+
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+
+	private String RoleName;
+
+	public String getRoleName() {
+		return RoleName;
+	}
+
+	public void setRoleName(String roleName) {
+		RoleName = roleName;
+	}
+}
---
+++ hcf/com/Drug/entity/Appsettings.java
@@ -0,0 +1,46 @@
+package com.Drug.entity;
+
+public class Appsettings {
+
+	private String cateGory;
+	
+	private String itemKey;
+	
+	private String description;
+	
+	private String ItemValue;
+
+	public String getCateGory() {
+		return cateGory;
+	}
+
+	public void setCateGory(String cateGory) {
+		this.cateGory = cateGory;
+	}
+
+	public String getItemKey() {
+		return itemKey;
+	}
+
+	public void setItemKey(String itemKey) {
+		this.itemKey = itemKey;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getItemValue() {
+		return ItemValue;
+	}
+
+	public void setItemValue(String itemValue) {
+		ItemValue = itemValue;
+	}
+	
+	
+}
