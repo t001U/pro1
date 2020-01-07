24d40dbbf5c1eb9896ddc46592f5fdf3cf2ac4eb
liyawei123 <2738872862@qq.com>
2020-01-07 11:26:44 +0800

    实体类

---
+++ src/entity/store.java
@@ -0,0 +1,116 @@
+package com.bdqn.entity;
+
+public class store {
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getCODE() {
+		return CODE;
+	}
+	public void setCODE(String cODE) {
+		CODE = cODE;
+	}
+	public String getDRUGID() {
+		return DRUGID;
+	}
+	public void setDRUGID(String dRUGID) {
+		DRUGID = dRUGID;
+	}
+	public String getCNAME() {
+		return CNAME;
+	}
+	public void setCNAME(String cNAME) {
+		CNAME = cNAME;
+	}
+	public String getSPEC() {
+		return SPEC;
+	}
+	public void setSPEC(String sPEC) {
+		SPEC = sPEC;
+	}
+	public String getUNIT() {
+		return UNIT;
+	}
+	public void setUNIT(String uNIT) {
+		UNIT = uNIT;
+	}
+	public String getJOBPRICE() {
+		return JOBPRICE;
+	}
+	public void setJOBPRICE(String jOBPRICE) {
+		JOBPRICE = jOBPRICE;
+	}
+	public String getSALEPRICE() {
+		return SALEPRICE;
+	}
+	public void setSALEPRICE(String sALEPRICE) {
+		SALEPRICE = sALEPRICE;
+	}
+	public int getNUMBER() {
+		return NUMBER;
+	}
+	public void setNUMBER(int nUMBER) {
+		NUMBER = nUMBER;
+	}
+	public int getUPLIMIT() {
+		return UPLIMIT;
+	}
+	public void setUPLIMIT(int uPLIMIT) {
+		UPLIMIT = uPLIMIT;
+	}
+	public int getDOWNLIMIT() {
+		return DOWNLIMIT;
+	}
+	public void setDOWNLIMIT(int dOWNLIMIT) {
+		DOWNLIMIT = dOWNLIMIT;
+	}
+	public int getTYPE() {
+		return TYPE;
+	}
+	public void setTYPE(int tYPE) {
+		TYPE = tYPE;
+	}
+	public String getPROVIDER() {
+		return PROVIDER;
+	}
+	public void setPROVIDER(String pROVIDER) {
+		PROVIDER = pROVIDER;
+	}
+	public String getBATCHID() {
+		return BATCHID;
+	}
+	public void setBATCHID(String bATCHID) {
+		BATCHID = bATCHID;
+	}
+	public String getTIMELIMIT() {
+		return TIMELIMIT;
+	}
+	public void setTIMELIMIT(String tIMELIMIT) {
+		TIMELIMIT = tIMELIMIT;
+	}
+	public String getINPUTCODE1() {
+		return INPUTCODE1;
+	}
+	public void setINPUTCODE1(String iNPUTCODE1) {
+		INPUTCODE1 = iNPUTCODE1;
+	}
+	private int ID;
+	private String CODE;
+	private String DRUGID;
+	private String CNAME;
+	private String SPEC;
+	private String UNIT;
+	private String JOBPRICE;
+	private String SALEPRICE;
+	private int NUMBER;
+	private int UPLIMIT;
+	private int DOWNLIMIT;
+	private int TYPE;
+	private String PROVIDER;
+	private String BATCHID;
+	private String TIMELIMIT;
+	private String INPUTCODE1;
+}
