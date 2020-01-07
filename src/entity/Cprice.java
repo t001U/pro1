2c2caebcfe2fe103b855f7b165f9b814e699cd2e
1413 <1413097437@qq.com>
2020-01-07 09:51:45 +0800

    价格表实体类

---
+++ src/entity/Cprice.java
@@ -0,0 +1,190 @@
+package com.Pro.entity;
+
+/**
+ * Cprice  ¼Û¸ñ±í
+ * @author 1
+ *
+ */
+public class Cprice {
+	private int ID;
+	
+	private char CODE;
+	
+	private String DRUGID;
+	
+	private String CNAME;
+	
+	private String SPEC;
+	
+	private String UNIT;
+	
+	private double JOBPRICE;
+	
+	private double SALEPRICE;
+	
+	private double NSALEPRICE;
+	
+	private int NUMBER;
+	
+	private String FILENO;
+	
+	private String PROVIDER;
+	
+	private int TYPE;
+	
+	private String TIMELIMIT;
+	
+	private String EVENTTIME;
+	
+	private String BATCHID;
+	
+	private String CAUSE;
+	
+	private String INPUTCODE1;
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public char getCODE() {
+		return CODE;
+	}
+
+	public void setCODE(char cODE) {
+		CODE = cODE;
+	}
+
+	public String getDRUGID() {
+		return DRUGID;
+	}
+
+	public void setDRUGID(String dRUGID) {
+		DRUGID = dRUGID;
+	}
+
+	public String getCNAME() {
+		return CNAME;
+	}
+
+	public void setCNAME(String cNAME) {
+		CNAME = cNAME;
+	}
+
+	public String getSPEC() {
+		return SPEC;
+	}
+
+	public void setSPEC(String sPEC) {
+		SPEC = sPEC;
+	}
+
+	public String getUNIT() {
+		return UNIT;
+	}
+
+	public void setUNIT(String uNIT) {
+		UNIT = uNIT;
+	}
+
+	public double getJOBPRICE() {
+		return JOBPRICE;
+	}
+
+	public void setJOBPRICE(double jOBPRICE) {
+		JOBPRICE = jOBPRICE;
+	}
+
+	public double getSALEPRICE() {
+		return SALEPRICE;
+	}
+
+	public void setSALEPRICE(double sALEPRICE) {
+		SALEPRICE = sALEPRICE;
+	}
+
+	public double getNSALEPRICE() {
+		return NSALEPRICE;
+	}
+
+	public void setNSALEPRICE(double nSALEPRICE) {
+		NSALEPRICE = nSALEPRICE;
+	}
+
+	public int getNUMBER() {
+		return NUMBER;
+	}
+
+	public void setNUMBER(int nUMBER) {
+		NUMBER = nUMBER;
+	}
+
+	public String getFILENO() {
+		return FILENO;
+	}
+
+	public void setFILENO(String fILENO) {
+		FILENO = fILENO;
+	}
+
+	public String getPROVIDER() {
+		return PROVIDER;
+	}
+
+	public void setPROVIDER(String pROVIDER) {
+		PROVIDER = pROVIDER;
+	}
+
+	public int getTYPE() {
+		return TYPE;
+	}
+
+	public void setTYPE(int tYPE) {
+		TYPE = tYPE;
+	}
+
+	public String getTIMELIMIT() {
+		return TIMELIMIT;
+	}
+
+	public void setTIMELIMIT(String tIMELIMIT) {
+		TIMELIMIT = tIMELIMIT;
+	}
+
+	public String getEVENTTIME() {
+		return EVENTTIME;
+	}
+
+	public void setEVENTTIME(String eVENTTIME) {
+		EVENTTIME = eVENTTIME;
+	}
+
+	public String getBATCHID() {
+		return BATCHID;
+	}
+
+	public void setBATCHID(String bATCHID) {
+		BATCHID = bATCHID;
+	}
+
+	public String getCAUSE() {
+		return CAUSE;
+	}
+
+	public void setCAUSE(String cAUSE) {
+		CAUSE = cAUSE;
+	}
+
+	public String getINPUTCODE1() {
+		return INPUTCODE1;
+	}
+
+	public void setINPUTCODE1(String iNPUTCODE1) {
+		INPUTCODE1 = iNPUTCODE1;
+	}
+	
+	
+}
