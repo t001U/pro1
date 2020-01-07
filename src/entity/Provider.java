ffe13e3f0173fe1fe220c457b2af1f5bce025242
1413 <1413097437@qq.com>
2020-01-07 09:53:49 +0800

    供应者实体类

---
+++ src/entity/Provider.java
@@ -0,0 +1,121 @@
+package com.Pro.entity;
+/**
+ *Provider	¹©Ó¦Õß
+ * @author 1
+ *
+ */
+public class Provider {
+	private int ID;
+	
+	private String NAME;
+	
+	private String ADDRESS;
+	
+	private String TEL;
+	
+	private String FAX;
+	
+	private String POSTCODE;
+	
+	private String EMAIL;
+	
+	private String CONTACT;
+	
+	private String PHONE;
+	
+	private String MOBILE;
+	
+	private String INPUTCODE;
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getNAME() {
+		return NAME;
+	}
+
+	public void setNAME(String nAME) {
+		NAME = nAME;
+	}
+
+	public String getADDRESS() {
+		return ADDRESS;
+	}
+
+	public void setADDRESS(String aDDRESS) {
+		ADDRESS = aDDRESS;
+	}
+
+	public String getTEL() {
+		return TEL;
+	}
+
+	public void setTEL(String tEL) {
+		TEL = tEL;
+	}
+
+	public String getFAX() {
+		return FAX;
+	}
+
+	public void setFAX(String fAX) {
+		FAX = fAX;
+	}
+
+	public String getPOSTCODE() {
+		return POSTCODE;
+	}
+
+	public void setPOSTCODE(String pOSTCODE) {
+		POSTCODE = pOSTCODE;
+	}
+
+	public String getEMAIL() {
+		return EMAIL;
+	}
+
+	public void setEMAIL(String eMAIL) {
+		EMAIL = eMAIL;
+	}
+
+	public String getCONTACT() {
+		return CONTACT;
+	}
+
+	public void setCONTACT(String cONTACT) {
+		CONTACT = cONTACT;
+	}
+
+	public String getPHONE() {
+		return PHONE;
+	}
+
+	public void setPHONE(String pHONE) {
+		PHONE = pHONE;
+	}
+
+	public String getMOBILE() {
+		return MOBILE;
+	}
+
+	public void setMOBILE(String mOBILE) {
+		MOBILE = mOBILE;
+	}
+
+	public String getINPUTCODE() {
+		return INPUTCODE;
+	}
+
+	public void setINPUTCODE(String iNPUTCODE) {
+		INPUTCODE = iNPUTCODE;
+	}
+	
+	
+	
+	
+}
