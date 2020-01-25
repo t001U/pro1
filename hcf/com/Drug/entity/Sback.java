2caaab3466738594d51d12d9c317bed6f94a043c
1413 <1413097437@qq.com>
2020-01-25 17:44:27 +0800

    销售退药

---
+++ hcf/com/Drug/entity/Sback.java
@@ -0,0 +1,153 @@
+package com.Pro.entity;
+
+import java.sql.Date;
+
+public class Sback {
+	/**
+	 * œ˙ €ÕÀ“©
+	 */
+	private int ID ;
+	
+	private int CODE ;
+	
+	private String DRUGID ;
+	
+	private String CNAME ;
+	
+	private String SPEC ;
+	
+	private String UNIT ;
+	
+	private int PACKRULE  ;
+	
+	private String JOBPRICE  ;
+	
+	private String SALEPRICE   ;
+	
+	private int NUMBER  ;
+	
+	private int TYPE  ;
+	
+	private String BATCHID   ;
+	
+	private String CUSTOMNAME    ;
+	
+	private Date TIMELIMIT ;
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public int getCODE() {
+		return CODE;
+	}
+
+	public void setCODE(int cODE) {
+		CODE = cODE;
+	}
+
+	public String getDRUGID() {
+		return DRUGID;
+	}
+
+	public void setDRUGID(String dRUGID) {
+		DRUGID = dRUGID;
+	}
+
+	public String getCNAME() {
+		return CNAME;
+	}
+
+	public void setCNAME(String cNAME) {
+		CNAME = cNAME;
+	}
+
+	public String getSPEC() {
+		return SPEC;
+	}
+
+	public void setSPEC(String sPEC) {
+		SPEC = sPEC;
+	}
+
+	public String getUNIT() {
+		return UNIT;
+	}
+
+	public void setUNIT(String uNIT) {
+		UNIT = uNIT;
+	}
+
+	public int getPACKRULE() {
+		return PACKRULE;
+	}
+
+	public void setPACKRULE(int pACKRULE) {
+		PACKRULE = pACKRULE;
+	}
+
+	public String getJOBPRICE() {
+		return JOBPRICE;
+	}
+
+	public void setJOBPRICE(String jOBPRICE) {
+		JOBPRICE = jOBPRICE;
+	}
+
+	public String getSALEPRICE() {
+		return SALEPRICE;
+	}
+
+	public void setSALEPRICE(String sALEPRICE) {
+		SALEPRICE = sALEPRICE;
+	}
+
+	public int getNUMBER() {
+		return NUMBER;
+	}
+
+	public void setNUMBER(int nUMBER) {
+		NUMBER = nUMBER;
+	}
+
+	public int getTYPE() {
+		return TYPE;
+	}
+
+	public void setTYPE(int tYPE) {
+		TYPE = tYPE;
+	}
+
+	public String getBATCHID() {
+		return BATCHID;
+	}
+
+	public void setBATCHID(String bATCHID) {
+		BATCHID = bATCHID;
+	}
+
+	public String getCUSTOMNAME() {
+		return CUSTOMNAME;
+	}
+
+	public void setCUSTOMNAME(String cUSTOMNAME) {
+		CUSTOMNAME = cUSTOMNAME;
+	}
+
+	public Date getTIMELIMIT() {
+		return TIMELIMIT;
+	}
+
+	public void setTIMELIMIT(Date tIMELIMIT) {
+		TIMELIMIT = tIMELIMIT;
+	}
+	
+	
+	
+	
+	
+}
